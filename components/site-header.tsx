1a70fe43ddbc5b965625fbf6a9ac82d01b772dfb
Ronaldo Dalle Vedove Cardoso <ronaldodallevedovecardoso@Ronaldos-MacBook-Air.local>
2026-07-26 14:30:34 -0300

    Center and simplify header navigation

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { BrandMark } from "@/components/brand-mark";
-import { CarIcon, HeartIcon, MenuIcon } from "@/components/icons";
+import { CarIcon, MenuIcon } from "@/components/icons";
 
 export function SiteHeader() {
   return (
@@ -11,17 +11,10 @@ export function SiteHeader() {
         <nav aria-label="Navigazione principale" className="desktop-nav">
           <Link href="/veicoli">Veicoli</Link>
           <Link href="/#marche">Marche</Link>
-          <Link href="/#selezioni">Selezioni</Link>
+          <Link href="/#servizio">Servizio</Link>
           <Link href="/#magazine">Magazine</Link>
         </nav>
         <div className="site-header__actions">
-          <Link
-            aria-label="Veicoli salvati"
-            className="icon-button desktop-only"
-            href="/veicoli"
-          >
-            <HeartIcon />
-          </Link>
           <Link className="button button--blue desktop-only" href="/#contatti">
             <CarIcon />
             Pubblica un veicolo
@@ -33,7 +26,7 @@ export function SiteHeader() {
             <nav aria-label="Navigazione mobile">
               <Link href="/veicoli">Veicoli</Link>
               <Link href="/#marche">Marche</Link>
-              <Link href="/#selezioni">Selezioni</Link>
+              <Link href="/#servizio">Servizio</Link>
               <Link href="/#magazine">Magazine</Link>
               <Link href="/#contatti">Pubblica un veicolo</Link>
             </nav>
